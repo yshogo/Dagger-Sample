c344a4168d33f9306f5cc00531c99bd6ecb2ca6a
yshogo <superman199323@gmail.com>
2018-10-23 16:00:58 +0900

    Databindingが動くことを確認

--- app/src/main/java/com/example/shogoyamada/dagger2_sample/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/shogoyamada/dagger2_sample/ui/main/MainFragment.kt
@@ -6,7 +6,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.shogoyamada.dagger2_sample.R
+import com.example.shogoyamada.dagger2_sample.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -15,16 +15,21 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: MainFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        binding = MainFragmentBinding.inflate(inflater, container, false).apply {
+            viewModel = this@MainFragment.viewModel
+        }
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
     }
 
 }
--- app/src/main/java/com/example/shogoyamada/dagger2_sample/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/shogoyamada/dagger2_sample/ui/main/MainViewModel.kt
@@ -3,5 +3,5 @@ package com.example.shogoyamada.dagger2_sample.ui.main
 import android.arch.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var test = "テストおおおおお"
 }
